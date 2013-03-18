6d4f740b96764dfbc712810a29637a99474c86a8
chadmichel <chadmichel@me.com>
2013-03-17 23:38:28 -0500

    serious cleanup

--- public/javascripts/korben.js
+++ public/javascripts/korben.js
@@ -1,63 +1,25 @@
 
 var Korben;
 (function(Korben) {
-
+	
 	// Wraps access to an IndexedDB database.
-	Korben.DbWrapper = function(db) {
+	Korben.DbWrapper = function(initFunction, dbName) {
 		var self = this;
-		self.db = db;
+		self.initFunction = initFunction;
+		self.dbName = dbName;
 		
 		self.store = function(storeName) {
-			return new Korben.StoreWrapper(db, storeName);					
+			return new Korben.StoreWrapper(self.initFunction, self.dbName, storeName);					
 		};
 	};
 	
 	// Wraps access to an IndexedDb store.
-	Korben.StoreWrapper = function(db, storeName) {
-		var self = this;		
-		self.db = db;
+	Korben.StoreWrapper = function(initFunction, dbName, storeName) {
+		var self = this;	
+		self.db = null;	
+		self.dbName = dbName;
 		self.storeName = storeName;
-				
-		self.put = function(object) {
-			
-			var def = $.Deferred();
-		
-			var tx = self.db.transaction(self.storeName, "readwrite");
-			var store = tx.objectStore(self.storeName);
-			var req = store.put(object);
-							
-			req.onsuccess = function(event) {
-				def.resolve();
-			};
-			req.onerror = function(event) {
-				def.reject(event);
-			};
-		
-			return def;
-		};
-		
-		self.get = function(id) {
-
-			var def = $.Deferred();
-			
-			var tx = db.transaction(self.storeName, "readwrite");
-			var store = tx.objectStore(self.storeName);
-			var req = store.get(id);
-					
-			req.onsuccess = function(event) {				
-				def.resolve(event.srcElement.result);
-			};
-			req.onerror = function(event) {
-				def.reject(event);
-			};
-
-			return def;
-		};
-	}
-
-	Korben.db = function(initFunction, dbName) {
-	
-		var def = $.Deferred();
+		self.promise = $.Deferred();
 		
 		var request = window.indexedDB.open(dbName, 1);      
 		var db = null;
@@ -68,16 +30,55 @@ var Korben;
         
 		request.onerror = function(event) {
 			// Do something with request.errorCode!
-			def.reject(event);
+			self.promise.reject(event);
 		};
 		request.onsuccess = function(event) {
 			// Do something with request.result!
-			db = event.target.result;
-			var store = new Korben.DbWrapper(db);
-			def.resolve(store);            
+			self.db = event.target.result;
+			self.promise.resolve();            
+		};
+						
+		self.put = function(object) {
+			
+			var def = $.Deferred();
+		
+			self.promise.then(function() { 
+				var tx = self.db.transaction(self.storeName, "readwrite");
+				var store = tx.objectStore(self.storeName);
+				var req = store.put(object);
+							
+				req.onsuccess = function(event) {
+					def.resolve();
+				};
+				req.onerror = function(event) {
+					def.reject(event);
+				};
+			});
+			return def;
 		};
 		
-		return def;
+		self.get = function(id) {
+
+			var def = $.Deferred();
+			
+			self.promise.then(function() { 
+				var tx = self.db.transaction(self.storeName, "readwrite");
+				var store = tx.objectStore(self.storeName);
+				var req = store.get(id);
+					
+				req.onsuccess = function(event) {				
+					def.resolve(event.srcElement.result);
+				};
+				req.onerror = function(event) {
+					def.reject(event);
+				};
+			});
+			return def;
+		};
+	}
+
+	Korben.db = function(initFunction, dbName) {	
+		return new Korben.DbWrapper(initFunction, dbName);
 	};
 	
 
