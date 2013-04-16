21219e7a52b0d5a946f45e119194472c5629b1cf
chadmichel <chadmichel@me.com>
2013-04-15 21:20:43 -0500

    add check for indexed support

--- public/javascripts/korben.js
+++ public/javascripts/korben.js
@@ -289,5 +289,22 @@ var Korben;
 		return new Korben.DbWrapper(initFunction, dbName);
 	};
 	
+	Korben.check = function(errFunction) {
+		// From: https://developer.mozilla.org/en-US/docs/IndexedDB/Using_IndexedDB
+		// In the following line, you should include the prefixes of implementations you want to test.
+		window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+		// DON'T use "var indexedDB = ..." if you're not in a function.
+		// Moreover, you may need references to some window.IDB* objects:
+		window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+		window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+		// (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
+
+		if (!window.indexedDB) {
+			if (errfunction != null) {
+				errFunction("No IndexedDB support.");
+			}					    
+		}
+	};
+	
 
 }) (Korben || (Korben = {}));
