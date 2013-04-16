25bcf0724c6675284afe1b046129451441a86188
chadmichel <chadmichel@me.com>
2013-04-15 23:34:56 -0500

    add more comments

--- public/javascripts/inittests.js
+++ public/javascripts/inittests.js
@@ -1,19 +1,31 @@
+
+// The init object is required to initialize your IndexedDB database.
+// Your init object must contain a version property version: 4 below.
+// Your init object must also have an update function.
 var initFunction = {			
 	
+	// Schema version of the database.
 	version: 4,
 	
+	// This function will be called when an upgrade of the database is required.
+	// You will have to write something like this function for your database.
+	// The event passed is an IndexedDB object, on that object you will have to
+	// create the object stores you intend to create.
 	upgrade: function(event) {
 		db = event.target.result;
 
 		if (event.oldVersion < 1) {
+			// If we are just getting started lets create a 'notes' store for unit testing.
 			var notesStore = db.createObjectStore("notes", { keyPath: "id" });
 			notesStore.createIndex("date", "date", { unique: false });
 		}	   
 		if (event.oldVersion < 2) {
+			// Add an integer store for unit testing.
 			var intidStore = db.createObjectStore("intid", { keyPath: "id" });			
 		}	   
 
 		if (event.oldVersion < 3) {
+			// Add a integer range store for unit testing.
 			var intRangeStore = db.createObjectStore("intrange", { keyPath: "id" });
 			intRangeStore.createIndex("intColumn", "intColumn", { unique: false });
 		}	   
--- public/javascripts/smoketests.js
+++ public/javascripts/smoketests.js
@@ -2,19 +2,29 @@
 
 asyncTest(" get / put ", function() {
 
+	// First we will setup a record to insert.
 	var id = UUID.generate();
 	var note = {id: id, title: "a title", date: new Date()};
 	
+	// Next we must open the database.
 	var db = Korben.db(initFunction, "SomeNotes");
+	// Then open the store.
 	var store = db.store("notes");
 
+	// Once we have a valid store we can put records into the store.
+	// put / get return a JQuery deferred object. Call then to wait
+	// for the operation to complete.
 	store.put(note).then(function() {
+		// Get takes a single parameter, the id of the record to find.
 		store.get(id).then(function(loaded) {
+			// then will be called once record is loaded.
+			// If record is not found loaded will be null.
 			ok(loaded.id === note.id);
 			start();
 		});
 	});
 	
+	// One assertion above (ok)
 	expect(1);
 });
 
