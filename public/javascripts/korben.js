ac965d2c761732efa96cbcdb8905a7b896a82a6e
chadmichel <chadmichel@me.com>
2013-04-17 23:51:44 -0500

    add start of using an AMD

--- public/javascripts/korben.js
+++ public/javascripts/korben.js
@@ -7,8 +7,9 @@
 //
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var Korben;
-(function(Korben) {
+(function() {
+	
+	var Korben = {};
 	
 	// Wraps access to an IndexedDB database.
 	// You will end accessing a store from this object.
@@ -306,5 +307,12 @@ var Korben;
 		}
 	};
 	
+	if (window.define == null) {
+		window.Korben = Korben;
+	}
+	else {
+		// AMD, lets use it
+		
+	}
 
-}) (Korben || (Korben = {}));
+}) ();
